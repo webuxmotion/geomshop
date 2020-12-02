0eb86542375e3517948996e27fdaf98d9d4a7576
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2020-12-02 22:42:01 +0200

    Firebase. createUserProfileDocument

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import { Header } from './components';
 import { HomePage, ShopPage, SigninPage } from './pages';
 
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends Component {
   state = {
@@ -14,8 +14,21 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+      } else {
+        this.setState({ currentUser: userAuth })
+      }
     })
   }
 
