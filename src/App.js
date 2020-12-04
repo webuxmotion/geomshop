ed0e523dac25498d686165f3617e286208273931
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2020-12-04 17:13:42 +0200

    Use REDUX for user

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import { Header } from './components';
 import { HomePage, ShopPage, SigninPage, SignupPage } from './pages';
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends Component {
-  state = {
-    currentUser: null
-  }
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
           });
         });
       } else {
-        this.setState({ currentUser: userAuth })
+        setCurrentUser(null);
       }
     })
   }
@@ -39,11 +37,18 @@ class App extends Component {
   render() {
     return (
       <>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/shop' component={ShopPage} />
-          <Route exact path='/signin' component={SigninPage} />
+          <Route exact path='/signin' render={() => 
+              this.props.currentUser ? (
+                <Redirect to='/' />
+              ) : (
+                <SigninPage />
+              )
+            } 
+          />
           <Route exact path='/signup' component={SignupPage} />
         </Switch>
       </>
@@ -51,4 +56,12 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = ({ user: { currentUser } }) => ({
+  currentUser
+});
+
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,5 @@
 import { Logo, MenuItem } from '../index';
+import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
 
@@ -30,4 +31,8 @@ const Header = ({ currentUser }) => (
   </header>
 );
 
-export default Header;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
