01c5e5b4336e5c29196fa22e9ec6e42f4ceb9cb5
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2020-12-15 19:11:08 +0200

    Use redux-persist

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './scss/imports.scss';
 import App from './App';
-import store from './redux/store';
+import { persistor, store } from './redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <React.StrictMode>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </React.StrictMode>
     </BrowserRouter>
   </Provider>,
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,25 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
-  cart: cartReducer,
+const rootPersistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['cart']
+}
+
+const cartPersistConfig = {
+  key: 'cart',
+  storage: storage,
+  blacklist: ['hidden']
+}
+
+const rootReducer = combineReducers({
+  cart: persistReducer(cartPersistConfig, cartReducer),
   user: userReducer
 });
+
+export default persistReducer(rootPersistConfig, rootReducer);
