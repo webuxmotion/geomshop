9fe777a203601574f48683570a7aa48bb4cd9595
Pereverziev Andrii <pereverziev.andrii@gmail.com>
2020-12-19 14:12:44 +0200

    useEffect in our app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -9,33 +9,30 @@ import { CheckoutPage, HomePage, ShopPage, SigninPage, SignupPage } from './page
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { checkUserSession } from './redux/user/user.actions';
 
-class App extends Component {
-  componentDidMount() {
-    const { checkUserSession } = this.props;
+const App = ({ checkUserSession, currentUser }) => {
+  useEffect(() => {
     checkUserSession();
-  }
+  }, [checkUserSession]);
   
-  render() {
-    return (
-      <>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-          <Route exact path='/checkout' component={CheckoutPage} />
-          <Route exact path='/signin' render={() => 
-              this.props.currentUser ? (
-                <Redirect to='/' />
-              ) : (
-                <SigninPage />
-              )
-            } 
-          />
-          <Route exact path='/signup' component={SignupPage} />
-        </Switch>
-      </>
-    );
-  }
+  return (
+    <>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route exact path='/checkout' component={CheckoutPage} />
+        <Route exact path='/signin' render={() => 
+            currentUser ? (
+              <Redirect to='/' />
+            ) : (
+              <SigninPage />
+            )
+          } 
+        />
+        <Route exact path='/signup' component={SignupPage} />
+      </Switch>
+    </>
+  );
 }
 
 const mapStateToProps = createStructuredSelector({
--- src/pages/shop-page/shop-page.js
+++ src/pages/shop-page/shop-page.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
@@ -6,31 +6,25 @@ import { CollectionOverviewContainer } from '../../components';
 import CollectionPageContainer from '../collection-page/collection-page.container';
 import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
-class ShopPage extends React.Component {
-  componentDidMount() {
-    const { fetchCollections } = this.props;
-    
+const ShopPage = ({ fetchCollections, match }) => {
+  useEffect(() => {
     fetchCollections();
-  }
+  }, [fetchCollections]);
   
-  render() {
-    const { match } = this.props;
-
-    return (
-      <div className="shop-page">
-        <Route 
-          exact 
-          path={`${match.path}`} 
-          component={CollectionOverviewContainer}
-        />
-        <Route 
-          path={`${match.path}/:collectionId`} 
-          component={CollectionPageContainer}
-        />
-      </div>
-    )
-  }
-};
+  return (
+    <div className="shop-page">
+      <Route
+        exact
+        path={`${match.path}`}
+        component={CollectionOverviewContainer}
+      />
+      <Route 
+        path={`${match.path}/:collectionId`}
+        component={CollectionPageContainer}
+      />
+    </div>
+  )
+}
 
 const mapDispatchToProps = dispatch => ({
   fetchCollections: () => dispatch(fetchCollectionsStart())
